5262867cc1bcffd25e1c6b58023792ab8e8c24cf
Lucas Teixeira <GibirinoGamer@hotmail.com>
2024-03-22 18:10:27 -0300

    Adicionando property e protegendo o código.

--- modelo.py
+++ modelo.py
@@ -1,21 +1,55 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
 
+    def dar_like(self):
+        self.__likes += 1
+    
+    @property
+    def likes(self):
+        return self.__likes
 
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+    
+    def dar_like(self):
+        self.__likes += 1
+    
+    @property
+    def likes(self):
+        return self.__likes
 
+    @property
+    def nome(self):
+        return self.__nome
 
-vingadores = Filme('Vingadores - Ultimato',2018, 160)
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
+vingadores = Filme('vingadores - ultimato',2018, 160)
 
-chosen = Serie('The Chosen', 2020, 3)
+chosen = Serie('the chosen', 2020, 3)
 
-print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}")
-print(f"Nome: {chosen.nome} - Ano: {chosen.ano} - Temporadas: {chosen.temporadas}")
+vingadores.dar_like()
+
+chosen.dar_like()
+
+chosen.dar_like()
+
+print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
+print(f"Nome: {chosen.nome} - Ano: {chosen.ano} - Temporadas: {chosen.temporadas} - Likes: {chosen.likes}")
