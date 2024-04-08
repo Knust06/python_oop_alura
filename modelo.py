7c8ca3e415b85421d532b0b49b71bcbf14c18d46
Lucas Teixeira <GibirinoGamer@hotmail.com>
2024-04-08 15:28:23 -0300

    Usando o polimorfismo para remover o if do código

--- modelo.py
+++ modelo.py
@@ -18,15 +18,22 @@ class Programa:
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
+    
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self._likes} likes')
 class Filme(Programa): #Herança seria criar uma classe que herda de outra que no caso a classe filme está herdando da classe Programa.
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes')
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+    def imprime(self):
+        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes')
 
 vingadores = Filme('vingadores - ultimato',2018, 160)
 
@@ -44,5 +51,5 @@ print(f"{chosen.nome} - {chosen.ano} - {chosen.temporadas} - {chosen.likes}")
 filmes_e_series = [vingadores, chosen]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f"{programa.nome} - {detalhes} - {programa.likes}")
+    for programa in filmes_e_series:
+        programa.imprime() #Polimorfismo é quando um objeto de uma classe filha é tratado como um objeto de uma classe pai. Usando a sobrescrita do método imprime().
